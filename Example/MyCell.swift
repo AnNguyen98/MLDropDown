396180f3be7b4c81c1cf49bd0a52ffb04321244a
AnNguyen98 <theannguyen98@gmail.com>
2023-01-11 22:37:01 +0700

    Add lib and config project

---
+++ Example/MyCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyCell.swift
+//  DropDown
+//
+//  Created by Kevin Hirsch on 17/08/16.
+//  Copyright © 2016 Kevin Hirsch. All rights reserved.
+//
+
+import UIKit
+import MLDropDown
+
+class MyCell: MLDropDownCell {
+	
+    @IBOutlet weak var logoImageView: UIImageView!
+	
+}
